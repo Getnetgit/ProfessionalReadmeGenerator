0d978089d352a7669d77044cb81e040050d09041
Get Ali <getnet4fafi@gmail.com>
2023-03-04 13:51:08 -0800

    adding code for Readme Generator app

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# ProfessionalReadmeGenerator
+# Professional Readme Generator
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+ ## Description
+
+The goal of this project is to build a command line application which will ask a series of questions and generate a quality readme. It is built based on a user story provided by UW for challenge 9. The full content of user story is in issue #1 of this repo . 
+
+ ## Usage
+
+ Application will be started by using the command node index.js then questions will be asked for project title, description, installation instructions, usage information, contribution guidelines, test instruction, license , gitHub username and email. One all questions are answered a readme file will be generated in the project directory using project title as a file name. See simulation video below.
+
+
+![Demo Vedio](./ReadmeGenerator%20Demo.gif)
+
+
+
+ ## Licences 
+
+Copyrite (c) 2023 getnetgit
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of<br>this software and associated documentation files (the “Software”), to deal in<br>the Software without restriction, including without limitation the rights to use,<br>copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the<br>Software, and to permit persons to whom the Software is furnished to do so,<br>subject to the following conditions:<br><br>The above copyright notice and this permission notice shall be included in all<br>copies or substantial portions of the Software.<br><br>THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,<br>EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES<br>OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND<br>NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT<br>HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,<br>WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING<br>FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR <br>OTHER DEALINGS IN THE SOFTWARE.
--- index.js
+++ index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
-
+const inquirer=require('inquirer')
+const generateMarkdown=require("./utils/generateMarkdown");
+const fs=require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is your project title?', 'Enter discription of your poroject','Enter installation for your app.','Enter usage information','Enter contribution guidelines','Enter test instructions','Select licence','What is your gitHub username?','What is your email address?'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let formatedFileName=`${fileName.split(' ').join('')}.md`;
+    fs.writeFile(formatedFileName,data,(err) => err ? console.error(err) : console.log('Success!'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt([
+        {
+        type:'input',
+        message:questions[0],
+        name: "title"
+        },
+    
+        {
+            type:'input',
+            message:questions[1],
+            name: "description"
+        },
+        {
+            type:'input',
+            message:questions[2],
+            name: "installation"
+        },
+        // {
+        //     type:'input',
+        //     message:questions[3],
+        //     name: "description"
+        // },
+        {
+            type:'input',
+            message:questions[3],
+            name: "usage"
+        },
+        {
+            type:'input',
+            message:questions[4],
+            name: "contributing"
+        },
+        {
+            type:'input',
+            message:questions[5],
+            name: "test"
+        },
+        {
+            type:'input',
+            message:questions[7],
+            name: "gitHubUsername"
+        },
+        {
+            type:'input',
+            message:questions[8],
+            name: "email"
+        },
+        {
+            type:'list',
+            message:questions[6],
+            name:'licences',
+            choices:["MIT",'Apache_2.0','GPLv3','BSD_3_Clause','none']
+
+        }
+    ]).then(responce=>{
+     console.log(responce)
+     //generateMarkdown(responce)
+     writeToFile(responce.title, generateMarkdown(responce))
+     console.log(generateMarkdown(responce))
+    })
+}
 
 // Function call to initialize app
 init();
